8968e96d761578cd0b01fd984f36d6af8661c183
fawwazali14 <fawwazali10@gmail.com>
2023-11-25 14:05:36 -0600

    ree

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask
+import os
 
 app = Flask(__name__)
 
@@ -7,4 +8,5 @@ def hello():
     return 'Hello, this is your Flask endpoint!'
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))  # Use 5000 as default if PORT is not set
+    app.run(debug=True, port=port)
