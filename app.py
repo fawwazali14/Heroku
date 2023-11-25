4d7a566dc1029fc59db47846994a2d6ff6756154
fawwazali14 <fawwazali10@gmail.com>
2023-11-25 14:09:27 -0600

    ree

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
-from flask import Flask
 import os
+from flask import Flask
 
 app = Flask(__name__)
 
 @app.route('/')
-def hello():
-    return 'Hello, this is your Flask endpoint!'
+def index():
+    return 'Hello, World!'
 
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 5000))  # Use 5000 as default if PORT is not set
-    app.run(debug=True, port=port)
+    port = int(os.environ.get('PORT', 5000))  # Use port provided by Heroku or default to 5000
+    app.run(host='0.0.0.0', port=port)
